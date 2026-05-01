d0f2f32d6597876f3011a99220a0d3006ea9d5f1
chahine <chahine@ordinateur-portable-de-chahine.home>
2026-05-01 04:51:36 +0200

    v83 PWA

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE = 'stagely-v1';
+const ASSETS = ['/'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
